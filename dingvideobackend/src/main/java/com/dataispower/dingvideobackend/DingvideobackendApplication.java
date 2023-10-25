911bcf55217b36ad4c138bdcc1b8569907d95a78
Tamiflu233 <1714118250@qq.com>
2023-10-25 17:15:36 +0800

    初始化项目架构

---
+++ dingvideobackend/src/main/java/com/dataispower/dingvideobackend/DingvideobackendApplication.java
@@ -0,0 +1,13 @@
+package com.dataispower.dingvideobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingvideobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingvideobackendApplication.class, args);
+    }
+
+}
---
+++ dingvideobackend/src/test/java/com/dataispower/dingvideobackend/DingvideobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataispower.dingvideobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingvideobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
