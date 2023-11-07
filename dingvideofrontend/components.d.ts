bf6c5105b7d5a3d515b66c1ec6a3f5cb4f0bc5ab
Tamiflu233 <1714118250@qq.com>
2023-11-07 14:08:10 +0800

    s

--- dingvideofrontend/components.d.ts
+++ dingvideofrontend/components.d.ts
@@ -17,8 +17,6 @@ declare module 'vue' {
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
